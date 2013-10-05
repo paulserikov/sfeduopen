f67d1abc6fc9a6cb9dc412903b7bbb8f7685da9c
Pavel Serikov <pavel.p.serikov@gmail.com>
2013-10-05 17:33:24 +0400

    Modify sfedunews.py for working with english page id's, for example id=4014

--- sfedunews.py
+++ sfedunews.py
@@ -3,7 +3,10 @@
 # last news id 45341
 # 404 page may not be as 404 apache page
 # http://sfedu.ru/www/sfedu$news$.show_full?p_nws_id=43952
+# working page
+
 # redir http://sfedu.ru/404.shtml
+# errors: 4014 (en)
 
 import time
 import urllib
@@ -16,20 +19,25 @@ start_time = time.time()
 
 f = open('sfedupostnames.txt', 'w')
 
-for i in range (0,46000):
+for i in range (4015,46000):
 	page = urllib.urlopen("http://sfedu.ru/www/sfedu$news$.show_full?p_nws_id="+str(i))
 	soup = BeautifulSoup(page.read(), fromEncoding="win-1251")
-	if soup.find('h1').string == "Ошибка 404".decode('utf-8'):
-		print "Страницы с id = "+str(i)+" не существует"
-		ce +=1 		
+	print i
+
+	if soup.find('h1'):
+		if soup.find('h1').string == "Ошибка 404".decode('utf-8'):
+			print "Страницы с id = "+str(i)+" не существует"
+			ce +=1
+		else:
+			A = soup.find('div', attrs={'class': 'content_top'}).h1.string
+			B = soup.find('div', attrs={'class': 'content_bottom_date'}).string 
+			print A
+			print B
+			f.write(str(i)+'\t'+str(B)+'\t'+str(A)+'\n')
+			cs +=1	
 	else:
-		cs +=1
-		A = soup.find('div', attrs={'class': 'content_top'}).h1.string
-		B = soup.find('div', attrs={'class': 'content_bottom_date'}).string
-		print i
-		print A
-		print B
-		f.write(str(i)+'\t'+str(B)+'\t'+str(A)+'\n')
+		print "Maybe English version"
+git c
 
 Total = time.time() - start_time, "seconds"
 print Total
